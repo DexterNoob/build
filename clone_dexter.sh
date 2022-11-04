8dfffab6d5d0e17bbe222a3daf7e33ecef2cff99
dexter <115158271+DexterNoob@users.noreply.github.com>
2022-11-04 19:47:26 +0700

    update dtc source

--- clone_dexter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dexter="$(pwd)/dexter"
-
-if [ ! -d $dexter ]; then
-    wget "$(curl -s https://raw.githubusercontent.com/noobbeast/dexter-clang/main/16.0.0/link.txt)" -O "DexterNoob-Clang-16.0.0.tar.gz"
-    mkdir dexter && tar -xf DexterNoob-Clang-16.0.0.tar.gz -C dexter && rm -rf DexterNoob-Clang-16.0.0.tar.gz link.txt 
-fi
