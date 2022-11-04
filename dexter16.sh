ef3b39e6baf4dded9f9b7d1e8ce6a30d08de0b94
dexter <115158271+DexterNoob@users.noreply.github.com>
2022-11-04 19:49:18 +0700

    update dtc

--- dexter16.sh
+++ dexter16.sh
@@ -2,7 +2,9 @@
 
 # Defined path
 MainPath="$(pwd)"
-dexter="$(pwd)/../dexter"
+dtc="$(pwd)/../DragonTC"
+gcc64="$(pwd)/../gcc64"
+gcc="$(pwd)/../gcc"
 Any="$(pwd)/../AnyKernel3"
 
 # Make flashable zip
@@ -19,7 +21,7 @@ MakeZip() {
     fi
     cp -af $MainPath/out/arch/arm64/boot/Image.gz-dtb $Any
     sed -i "s/kernel.string=.*/kernel.string=$KERNEL_NAME-$HeadCommit test by $KBUILD_BUILD_USER/g" anykernel.sh
-    zip -r9 $MainPath/"$KERNEL_NAME-$Compiler-$TIME.zip" * -x .git README.md *placeholder
+    zip -r9 $MainPath/"[$Compiler][R-OSS]-$ZIP_KERNEL_VERSION-$KERNEL_NAME-$TIME.zip" * -x .git README.md *placeholder
     cd $MainPath
 }
 
@@ -27,31 +29,28 @@ MakeZip() {
 HeadCommit="$(git log --pretty=format:'%h' -1)"
 export ARCH="arm64"
 export SUBARCH="arm64"
-export KBUILD_BUILD_USER="Noobbeast"
-export KBUILD_BUILD_HOST="Github-Action"
+export KBUILD_BUILD_USER="noobbeast"
+export KBUILD_BUILD_HOST="GithubServer"
 Defconfig="begonia_user_defconfig"
 KERNEL_NAME=$(cat "$MainPath/arch/arm64/configs/$Defconfig" | grep "CONFIG_LOCALVERSION=" | sed 's/CONFIG_LOCALVERSION="-*//g' | sed 's/"*//g' )
 ZIP_KERNEL_VERSION="4.14.$(cat "$MainPath/Makefile" | grep "SUBLEVEL =" | sed 's/SUBLEVEL = *//g')$(cat "$(pwd)/Makefile" | grep "EXTRAVERSION =" | sed 's/EXTRAVERSION = *//g')"
 TIME=$(date +"%m%d%H%M")
 
 # Start building
-Compiler=Begonia-Thin
+Compiler=DTC
 MAKE="./makeparallel"
 rm -rf out
 BUILD_START=$(date +"%s")
 
 make  -j$(nproc --all)  O=out ARCH=arm64 SUBARCH=arm64 $Defconfig
 make  -j$(nproc --all)  O=out \
-                        PATH="$dexter/bin:/usr/bin:$PATH" \
+                        PATH="$dtc/bin:/$gcc64/bin:/$gcc/bin:/usr/bin:$PATH" \
+                        LD_LIBRARY_PATH="$dtc/lib64:$LD_LIBRABRY_PATH" \
                         CC=clang \
-                        AS=llvm-as \
-                        NM=llvm-nm \
-                        OBJCOPY=llvm-objcopy \
-                        OBJDUMP=llvm-objdump \
-                        STRIP=llvm-strip \
                         LD=ld.lld \
-                        CROSS_COMPILE=aarch64-linux-gnu- \
-                        CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                        CROSS_COMPILE=aarch64-linux-android- \
+                        CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                        CLANG_TRIPLE=aarch64-linux-gnu- \
                         2>&1 | tee out/error.log
 
 if [ -e $MainPath/out/arch/arm64/boot/Image.gz-dtb ]; then
