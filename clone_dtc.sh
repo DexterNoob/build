47ebd23280a81a604aa9b76c22a979e35795597e
dexter <115158271+DexterNoob@users.noreply.github.com>
2022-11-04 20:43:31 +0700

    Update clone_dtc.sh

--- clone_dtc.sh
+++ clone_dtc.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 dtc="$(pwd)/DragonTC"
 gcc64="$(pwd)/gcc64"
 gcc="$(pwd)/gcc"
@@ -6,9 +8,8 @@ if [ ! -d $dtc ]; then
     git clone --depth=1 https://github.com/TeraaBytee/DragonTC $dtc
 fi
 if [ ! -d $gcc64 ]; then
-    git clone --depth=1https://github.com/noobbeast/aarch64-linux-android-4.9.git -b main $gcc64
+    git clone --depth=1 https://github.com/TeraaBytee/aarch64-linux-android-4.9 $gcc64
 fi
 if [ ! -d $gcc ]; then
-    git clone --depth=1 https://github.com/noobbeast/arm-linux-androideabi-4.9.git -b main $gcc
+    git clone --depth=1 https://github.com/TeraaBytee/arm-linux-androideabi-4.9 $gcc
 fi
-
